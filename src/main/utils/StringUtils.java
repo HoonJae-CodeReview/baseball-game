896b2b675740a1b604affb3b11664ed0b505be84
SJ70 <seojae70@gmail.com>
2023-09-19 23:17:13 +0900

    Feat: String을 숫자 배열로 파싱하는 util 메소드 작성

---
+++ src/main/utils/StringUtils.java
@@ -0,0 +1,17 @@
+package main.utils;
+
+public final class StringUtils {
+
+    private StringUtils() {}
+
+    int[] parseToNumbers(String stringValue) {
+        int length = stringValue.length();
+        int[] numbers = new int[length];
+        for (int i=0; i<length; i++) {
+            int number = stringValue.charAt(i) - '0';
+            numbers[i] = number;
+        }
+        return numbers;
+    }
+
+}
