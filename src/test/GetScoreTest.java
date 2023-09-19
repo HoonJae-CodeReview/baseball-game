ea377e9622e925aeb196c9e5deacaa62fd6b7361
SJ70 <seojae70@gmail.com>
2023-09-19 23:01:56 +0900

    Chore: 테스트 클래스 이름 변경

--- src/test/GetScoreTest.java
+++ src/test/GetScoreTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class NumbersSBOTest {
+public class GetScoreTest {
 
     @ParameterizedTest()
     @CsvSource(
