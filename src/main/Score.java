298fcca817dee73fc48c0075742910e7e56022b6
SJ70 <seojae70@gmail.com>
2023-09-19 22:59:28 +0900

    Feat: SBO 점수를 저장하는 클래스 Score 추가

---
+++ src/main/Score.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Score {
+    private final int strikeCount, ballCount, outCount;
+
+    public Score(int strikeCount, int ballCount, int outCount) {
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
+        this.outCount = outCount;
+    }
+}
