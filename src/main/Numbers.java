eb20dc2bb570e4e01f96b8bcbe2cae2b01d06859
SJ70 <seojae70@gmail.com>
2023-09-19 22:05:27 +0900

    Feat: 한 번에 선택된 숫자들에 대한 클래스 Numbers 추가

---
+++ src/main/Numbers.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Numbers {
+    final int[] numbers;
+    final Map<Integer, Integer> numberCounts;
+
+    public Numbers(int[] values) {
+        numbers = values;
+        numberCounts = getNumberCountsFromArray(values);
+    }
+
+    private HashMap<Integer, Integer> getNumberCountsFromArray(int[] values) {
+        HashMap<Integer, Integer> balls = new HashMap<>();
+        for (int value : values){
+            int numberCountOfCurrentValue = balls.getOrDefault(value, 0);
+            balls.put(value, numberCountOfCurrentValue + 1);
+        }
+        return balls;
+    }
+
+}
