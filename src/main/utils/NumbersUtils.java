c1d99ad98ab91f350af4ef2221dca56b4e3b7285
SJ70 <seojae70@gmail.com>
2023-09-22 10:01:29 +0900

    Refactor: 난수 생성 메소드를 Stream으로 구현

--- src/main/utils/NumbersUtils.java
+++ src/main/utils/NumbersUtils.java
@@ -1,30 +1,17 @@
 package main.utils;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
 
 public class NumbersUtils {
     
     private NumbersUtils() {}
 
     public static List<Integer> getRandomizedNumbers(int length) {
-        List<Integer> result = new ArrayList<>();
-
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i=1; i<=9; i++) {
-            numbers.add(i);
-        }
-
-        for (int i=0; i<length; i++) {
-            int leftNumbersCount = numbers.size();
-            int randomIndex = (int) Math.floor(Math.random() * leftNumbersCount);
-            int randomNumber = numbers.get(randomIndex);
-            numbers.remove(randomIndex);
-            result.add(randomNumber);
-        }
-        return result;
+        return Stream.generate(() -> new Random().nextInt(9) + 1)
+                .distinct()
+                .limit(length)
+                .toList();
     }
-
-
-    
 }
