ab6294aa25ab69636f527e282dae393c2d759124
SJ70 <seojae70@gmail.com>
2023-09-22 10:16:11 +0900

    Refactor: 조건문 묶기

--- src/main/validators/ContinueOrExitValidator.java
+++ src/main/validators/ContinueOrExitValidator.java
@@ -5,9 +5,13 @@ public final class ContinueOrExitValidator {
     private ContinueOrExitValidator() {}
 
     public static void validateContinueOrExitResponse(String stringValue) {
-        if (!stringValue.equals("1") && !stringValue.equals("2")) {
+        if (!isCorrectInputValue(stringValue)) {
             throw new IllegalArgumentException("제시된 항목에서 선택해주세요.");
         }
+    }
+
+    private static boolean isCorrectInputValue(String stringValue) {
+        return stringValue.equals("1") || stringValue.equals("2");
     }
 
 }
