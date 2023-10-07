30cd85cd943f1fb568d7ccb4837cf7f2d6f69473
SJ70 <seojae70@gmail.com>
2023-10-07 22:11:26 +0900

    Feat: List<Integer>을 List<Number_>로 변환하는 유틸 함수

--- src/main/utils/NumbersUtils.java
+++ src/main/utils/NumbersUtils.java
@@ -1,12 +1,22 @@
 package main.utils;
 
+import main.Number_;
+
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class NumbersUtils {
     
     private NumbersUtils() {}
+
+    public static List<Number_> convertToNumberList(List<Integer> values) {
+        AtomicInteger position = new AtomicInteger(1);
+        return values.stream()
+                .map(value -> new Number_(value, position.getAndIncrement()))
+                .toList();
+    }
 
     public static List<Integer> getRandomizedNumbers(int length) {
         return Stream.generate(() -> new Random().nextInt(9) + 1)
