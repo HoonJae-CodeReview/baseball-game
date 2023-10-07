d25a6c891b3d2189d67340287f45c2ae958121d6
SJ70 <seojae70@gmail.com>
2023-10-07 20:32:40 +0900

    Feat: 숫자 하나의 값과 위치를 저장하는 클래스

---
+++ src/main/Number_.java
@@ -0,0 +1,13 @@
+package main;
+
+
+public class Number_ {
+
+    private final int value;
+    private final int position;
+
+    public Number_(int value, int position) {
+        this.value = value;
+        this.position = position;
+    }
+}
